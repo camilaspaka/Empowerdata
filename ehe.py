221742ca1b8c2fada67d4b90b22689f22daea281
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-18 11:29:43 -0300

    Orçamento
    Projeto da imersão da Empowerdata
    Inicialmente seria com o Jupyternotebook para que eu aprenda o utilizar também

--- ehe.py
+++
@@ -1,4 +0,0 @@
-projeto = input("Digite a descrição do projeto: ")
-horas_estimadas = input("Digite o total de horas estimadas: ")
-valor_hora = input("Digite o valor da hora trabalhada: ")
-prazo_estimado = input("Digite o prazo estimado: ")
