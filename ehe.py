e159e8f860b3cbc428e53d5b19f07cd8de4f2028
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-18 11:20:19 -0300

    Create ehe.py

---
+++ ehe.py
@@ -0,0 +1,4 @@
+projeto = input("Digite a descrição do projeto: ")
+horas_estimadas = input("Digite o total de horas estimadas: ")
+valor_hora = input("Digite o valor da hora trabalhada: ")
+prazo_estimado = input("Digite o prazo estimado: ")
